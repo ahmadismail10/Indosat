78215cacb3ce9bbc4e37696d3a055c9548951a47
Hendra Darmawan <zavoxcool@gmail.com>
2017-10-08 11:21:00 +0700

    Update view and procedure escendol.sql

--- view and procedure escendol.sql
+++ view and procedure escendol.sql
@@ -1,6 +1,6 @@
+--Sheet 1 Nomor 1
 DROP TABLE IF EXISTS `growth_vlr`;
 CREATE TABLE IF NOT EXISTS `growth_vlr` (
-  `nama_csm` varchar DEFAULT NULL;
   `tanggal` date DEFAULT NULL,
   `vlr` int(11) DEFAULT NULL,
   `vlr_sebelumnya` int(11) DEFAULT NULL,
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS `growth_vlr` (
   `keterangan` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 DELIMITER $$
+
 CREATE OR REPLACE PROCEDURE delete_growth_vlr(IN keyword date)
 	BEGIN
 		DELETE FROM `growth_vlr` WHERE MONTH(`tanggal`) = MONTH(keyword) AND YEAR(`tanggal`) = YEAR(keyword);
@@ -15,7 +16,6 @@ CREATE OR REPLACE PROCEDURE delete_growth_vlr(IN keyword date)
 CREATE OR REPLACE PROCEDURE growth_vlr(IN keyword date) 
 	BEGIN 
 		DECLARE counter INT;
-		DECLARE nama_csm varchar;
 		DECLARE tanggal date; 
 		DECLARE vlr INT;
 		DECLARE vlr_sebelumnya INT;
@@ -39,24 +39,34 @@ CREATE OR REPLACE PROCEDURE growth_vlr(IN keyword date)
 						ELSE
 							SET keterangan = 'turun';
 						END IF;
-						INSERT INTO growth_vlr VALUES(nama_csm, tanggal, vlr, vlr_sebelumnya, (concat(round(( abs(vlr-vlr_sebelumnya)/vlr_sebelumnya * 100 ),2),'%')), keterangan);
+						INSERT INTO growth_vlr VALUES(tanggal, vlr, vlr_sebelumnya, (concat(round(( abs(vlr-vlr_sebelumnya)/vlr_sebelumnya * 100 ),2),'%')), keterangan);
 					END IF;
 				end loop looping; 
 			close cur1; 
-			SELECT v.nama_csm, v.tanggal, v.vlr, v.vlr_sebelumnya, v.persentase, v.keterangan FROM (SELECT MAX(x.vlr) AS max FROM growth_vlr x WHERE MONTH(x.tanggal) = MONTH(keyword) AND YEAR(x.tanggal) = YEAR(keyword)) m, growth_vlr v WHERE MONTH(v.tanggal) = MONTH(keyword) AND YEAR(v.tanggal) = YEAR(keyword) AND v.vlr = m.max;
+			SELECT v.tanggal, v.vlr, v.vlr_sebelumnya, v.persentase, v.keterangan FROM (SELECT MAX(x.vlr) AS max FROM growth_vlr x WHERE MONTH(x.tanggal) = MONTH(keyword) AND YEAR(x.tanggal) = YEAR(keyword)) m, growth_vlr v WHERE MONTH(v.tanggal) = MONTH(keyword) AND YEAR(v.tanggal) = YEAR(keyword) AND v.vlr = m.max;
 		END IF;
 	END$$
 	
+						
+--Sheet 1 Nomor 5
 SELECT ORGANIZATION_NAME, MAX(AMOUNT_DEBIT) 
 FROM 'TRX_OUTLET';
 
+--Sheet 1 Nomor 7
 SELECT CELLID, MIN(HSDPA_USER_W36)
 FROM `UTILISASI_SA_SURABAYA` 
 WHERE MICRO_CLUSTER='MC-Surabaya_1_1';
 
+--Sheet 1 Nomor 8
 SELECT ORGANIZATION_ID, ORGANIZATION_NAME
 FROM `TRX_OUTLET` 
 WHERE MONTH(TANGGAL)=9 
 GROUP BY ORGANIZATION_ID 
 ORDER BY AVG(AMOUNT_DEBIT) ASC 
 LIMIT 5;
+
+--Sheet 2 Nomor 1.1
+SELECT NAMA_CSM, TANGGAL 
+FROM `vlr` 
+WHERE MICRO_CLUSTER='MC-SURABAYA_1_1' AND DAYNAME(TANGGAL)='WEDNESDAY'
+GROUP BY TANGGAL;
